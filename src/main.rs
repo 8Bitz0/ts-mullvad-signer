c1f0ba3d8574096978dd084d69d947d895891570
Alexander Hansen <alex@bitzero.me>
2025-01-23 11:19:31 -0500

    Use `console` for styles within terminal output
    Signed-off-by: Alexander Hansen <alex@bitzero.me>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod tailscale;
 
 use clap::Parser;
+use console::{style, Term};
 use indicatif::{ProgressBar, ProgressStyle};
+use std::time::Duration;
 
 #[derive(Parser)]
 struct Args {
@@ -18,15 +20,19 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+
+    let term = Term::stdout();
+    let term_err = Term::stderr();
     
     let spinner = ProgressBar::new_spinner();
     spinner.set_message("Fetching nodes...");
+    spinner.enable_steady_tick(Duration::from_millis(100));
     
     let status = match tailscale::LockStatus::fetch_from_cli() {
         Ok(status) => status,
         Err(e) => {
             spinner.abandon_with_message("Failed to fetch nodes");
-            eprintln!("Error fetching Tailscale lock status: {}", e);
+            error(&term_err, format!("Error fetching Tailscale lock status: {}", e));
             std::process::exit(1);
         }
     };
@@ -35,21 +41,22 @@ fn main() {
     
     let spinner = ProgressBar::new_spinner();
     spinner.set_message("Selecting nodes...");
+    spinner.enable_steady_tick(Duration::from_millis(100));
     
     let nodes: Vec<(String, String)> = status.select_mullvad_nodes(args.resign);
     
     spinner.finish_with_message("Nodes selected");
     
     if nodes.is_empty() {
-        eprintln!("No filtered Mullvad nodes found. Make sure your device is authorized\nto access Mullvad nodes and that they aren't already signed.");
+        error(&term_err, "No filtered Mullvad nodes found. Make sure your device is authorized\nto access Mullvad nodes and that they aren't already signed.");
         std::process::exit(1);
     }
     
     if !args.no_print {
-        println!("Nodes:");
+        term.write_line("Nodes:").unwrap();
         
         for n in &nodes {
-            println!("- {}: {}", n.1, n.0);
+            term.write_line(&format!("- {}: {}", n.1, n.0)).unwrap();
         }
         
         println!();
@@ -57,19 +64,19 @@ fn main() {
     
     println!("Selected {} node{}", nodes.len(), if nodes.len() == 1 { "" } else { "s" });
     println!("These nodes have been selected by checking for the node name suffix.");
-    println!("By signing these nodes, you trust them to interact with your tailnet.");
-    println!("The signing process may take a few minutes to complete.");
+    println!("{}", style("By signing these nodes, you trust them to interact with your tailnet.").bold());
+    println!("The signing process may take several minutes to complete.");
     
     if !args.yes {
         let dialog = dialoguer::Confirm::new()
-            .with_prompt("Sign ALL selected nodes?")
+            .with_prompt(format!("Sign {} selected nodes?", style("ALL").bold()))
             .default(false)
             .show_default(true)
             .interact()
             .unwrap();
         
         if !dialog {
-            println!("Aborting...");
+            error(&term_err, "Aborting...");
             std::process::exit(0);
         }
     }
@@ -77,16 +84,17 @@ fn main() {
     let progress_bar = ProgressBar::new(nodes.len() as u64);
     progress_bar.set_message("Signing nodes...");
     progress_bar.set_style(ProgressStyle::default_bar()
-        .template("{spinner} [{elapsed_precise}] [{bar:40}] {pos}/{len} ({eta})")
+        .template("{spinner} [{elapsed_precise}] [{bar:40.green}] {pos}/{len} ({eta})")
         .unwrap()
         .progress_chars("#> "));
+    progress_bar.enable_steady_tick(Duration::from_millis(100));
     
     for n in nodes {
         match tailscale::sign_node(&n.0) {
             Ok(_) => {},
             Err(e) => {
-                progress_bar.abandon_with_message("Failed signing nodes");
-                eprintln!("Error signing node: {} (node key: {})", e, n.0);
+                progress_bar.abandon_with_message("Failed signing nodes");;
+                error(&term_err, format!("Error signing node: {} (node key: {})", e, n.0));
                 std::process::exit(1);
             }
         };
@@ -96,6 +104,10 @@ fn main() {
     
     progress_bar.finish_with_message("Signed nodes");
     
-    println!("All detected Mullvad nodes should now be signed.");
-    println!("You may need to sign additional nodes over time as available Mullvad\nservers change (either manually or by re-running this tool.)");
+    term.write_line("All detected Mullvad nodes should now be signed.").unwrap();
+    term.write_line("You may need to sign additional nodes over time as available Mullvad\nservers change (either manually or by re-running this tool.)").unwrap();
+}
+
+fn error(stderr: &Term, msg: impl std::fmt::Display) {
+    stderr.write_line(&style(msg.to_string()).red().to_string()).unwrap();
 }
